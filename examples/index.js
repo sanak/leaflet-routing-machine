b2af395c04667d06d5ee6a667f289e81752a2031
Ko Nagase <nagase@georepublic.co.jp>
2015-09-16 17:23:07 +0900

    adjusting examples/index.js

--- examples/index.js
+++ examples/index.js
@@ -7,14 +7,14 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 L.Routing.control({
 	waypoints: [
 		L.latLng(34.944479, 135.702857),
-		//L.latLng(34.944916, 135.702972),
+		L.latLng(34.944916, 135.702972),
 		L.latLng(34.944530, 135.703476)
 	],
 	//router: L.Routing.graphHopper('f48ee041-ed02-4331-801a-a5b11514707c'),
 	//router: L.Routing.mapbox('pk.eyJ1IjoibmFnYXNlIiwiYSI6IjZnYzBkT3MifQ.ckZKcY6JKHeIylxa0PWa6g'),
 	router: L.Routing.pgRouting('muko'),
 	geocoder: L.Control.Geocoder.nominatim(),
-    routeWhileDragging: true,
+    routeWhileDragging: false,
     reverseWaypoints: true,
     showAlternatives: true,
     altLineOptions: {
